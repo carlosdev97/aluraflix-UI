5b52e88357a11ea66c9fef841b823439296d9d0f
Carlosedm97 <carlosedm97@gmail.com>
2023-05-29 16:55:17 -0500

    Finalizado el diseño responsivo del proyecto.

--- src/components/Carousel/Slider/Slider.jsx
+++ src/components/Carousel/Slider/Slider.jsx
@@ -9,10 +9,10 @@ import VideoCard from "../VideoCard/VideoCard";
 
 function SliderVideos({ categorie, videos }) {
 
-    function validateVideos(){
-        if (videos.length >= 3){
+    function validateVideos() {
+        if (videos.length >= 3) {
             return 3;
-        } else if (videos.length == 2){
+        } else if (videos.length == 2) {
             return 2;
         } else {
             return 1;
@@ -23,23 +23,49 @@ function SliderVideos({ categorie, videos }) {
         infinite: true,
         speed: 500,
         slidesToShow: validateVideos(),
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 0
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
-    
+
     return (
         <>
             {
-                videos.length > 0 && 
+                videos.length > 0 &&
                 <div className="slider__container">
                     <div className="slider__info">
-                        <h2 className="slider__title" style={{backgroundColor: categorie.color}}>{categorie.nombre}</h2>
+                        <h2 className="slider__title" style={{ backgroundColor: categorie.color }}>{categorie.nombre}</h2>
                         <div className="slider__description">{categorie.descripcion}</div>
                     </div>
                     <Slider {...settings} className="slider">
                         {
                             videos.map((video) => {
-                            const { videoUrl, imageUrl } = video;
-                            return <VideoCard videoUrl={ videoUrl } imageUrl={ imageUrl } color={categorie.color} />
+                                const { videoUrl, imageUrl } = video;
+                                return <VideoCard videoUrl={videoUrl} imageUrl={imageUrl} color={categorie.color} />
                             })
                         }
                     </Slider>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
     return (
         <footer className='footer'>
             <a className='footer__link' href='#'><img className='footer__logo' src={logoAluraflix} alt='Logo de Aluraflix'/></a>
+            <p class="copyright__text">Copyright &copy; 2023 <a class="copyright__link" href="https://github.com/Carlosedm97/" target="_blank" rel="noopener noreferrer">Carlos López</a></p>
         </footer>
     )
 }
